d74cfa9023d1e3afe8ea5fe478d30e2575c05dbf
Jyrki Alakuijala <jyrki@google.com>
2012-02-08 15:28:14 +0000

    reducing stl use in tools/png2webpll.cc

--- src/lossless/tools/png2webpll.cc
+++ src/lossless/tools/png2webpll.cc
@@ -11,11 +11,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
-#include <set>
 #include <string>
-#include <iterator>
-#include <fstream>
-#include <streambuf>
 #include <vector>
 #include "../dec/decode.h"
 #include "../enc/encode.h"
@@ -300,22 +296,60 @@ PngDecoder::~PngDecoder() {
   delete pimpl_;
 }
 
-static int UniquePixels(int n, const uint32_t *argb) {
-  std::set<uint32_t> k;
-  k.insert(argb[0]);
-  for (int i = 1; i < n; ++i) {
-    if (argb[i - 1] == argb[i]) {
+static int Uint32Order(const void *p1, const void *p2) {
+  uint32_t a = *(const uint32_t *)p1;
+  uint32_t b = *(const uint32_t *)p2;
+  if (a < b) {
+    return -1;
+  }
+  if (a == b) {
+    return 0;
+  }
+  return 1;
+}
+
+// TODO(jyrki): this is already private in webp_bit_stream.cc
+static bool CreatePalette256(int n, const uint32_t *argb,
+                             std::vector<uint32_t>* palette) {
+  int i;
+  const int max_palette_size = 256;
+  int current_size = 0;
+  uint8_t in_use[1024];
+  uint32_t colors[1024];
+  static const uint32_t kHashMul = 0x1e35a7bd;
+  memset(&in_use[0], 0, sizeof(in_use));
+  for (i = 0; i < n; ++i) {
+    if (i != 0 && argb[i - 1] == argb[i]) {
       continue;
     }
-    if (k.find(argb[i]) == k.end()) {
-      k.insert(argb[i]);
-      if (k.size() >= 65536) {
-        // enough...
+    int addr = (kHashMul * argb[i]) >> 22;
+    for(;;) {
+      if (!in_use[addr]) {
+        colors[addr] = argb[i];
+        in_use[addr] = 1;
+        ++current_size;
+        if (current_size > max_palette_size) {
+          return false;
+        }
         break;
+      } else if (colors[addr] == argb[i]) {
+        // The color is already there.
+        break;
+      } else {
+        // Some other color sits there.
+        // Do linear conflict resolution.
+        ++addr;
+        addr &= 0x3ff;
       }
     }
   }
-  return k.size();
+  for (i = 0; i < sizeof(in_use); ++i) {
+    if (in_use[i]) {
+      palette->push_back(colors[i]);
+    }
+  }
+  qsort(&(*palette)[0], palette->size(), sizeof((*palette)[0]), Uint32Order);
+  return true;
 }
 
 static std::string ReadFileToString(const char *path) {
@@ -517,7 +551,9 @@ int main(int argc, char **argv) {
   // 1: non-color-predicted mode
   // 2: mode with the small palette
 
-  int try_with_small_palette = UniquePixels(xsize * ysize, argb_orig) <= 256;
+  std::vector<uint32_t> palette;
+  int try_with_small_palette =
+      CreatePalette256(xsize * ysize, argb_orig, &palette);
   double nonpredicted_bits = 0.0;
   double predicted_bits = 0.0;
   IsPhotographic(xsize, ysize, argb_orig, &nonpredicted_bits, &predicted_bits);
