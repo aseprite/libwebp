dd7e704345f3fcfabc2888ba581a1ea030236ab4
Jyrki Alakuijala <jyrki@google.com>
2012-02-09 18:20:05 +0000

    migrating the PixelHasher C++ struct into a simple C89 struct

--- src/lossless/common/pixel_hasher.h
+++ src/lossless/common/pixel_hasher.h
@@ -19,50 +19,56 @@ static const uint32_t kHashMul = 0x1e35a7bd;
 
 // kNotInitialized is a special value which can be inserted into the
 // PixelHasher, but is never recalled as a value.
-static const uint32_t kNotInitialized = kHashMul;
+static const uint32_t kNotInitialized = 0x1e35a7bd;
 
 struct PixelHasher {
-  void Init(int hash_bits) {
-    int i;
-    hash_shift_ = 32 - hash_bits;
-    hash_size_ = 1 << hash_bits;
-    data_ = (uint32_t *)malloc(hash_size_ * sizeof(data_[0]));
-    for (i = 0; i < hash_size_; ++i) {
-      data_[i] = kNotInitialized;
-    }
-  }
-  void Delete() {
-    free(data_);
-  }
-  void insert(uint32_t argb) {
-    const uint32_t key = (kHashMul * argb) >> hash_shift_;
-    data_[key] = argb;
-  }
-  bool is_initialized(uint32_t argb) const {
-    const uint32_t key = (kHashMul * argb) >> hash_shift_;
-    return data_[key] != kNotInitialized;
-  }
-  bool contains(uint32_t argb) const {
-    uint32_t key;
-    if (argb == kNotInitialized) {
-      return false;
-    }
-    key = (kHashMul * argb) >> hash_shift_;
-    return data_[key] == argb;
-  }
-  bool lookup(uint32_t key, uint32_t* argb) {
-    assert(key < hash_size_);
-    if (data_[key] != kNotInitialized) {
-      *argb = data_[key];
-      return true;
-    }
-    return false;
-  }
-
   uint32_t *data_;
   uint32_t hash_shift_;
   uint32_t hash_size_;
 };
+
+static void VP8LPixelHasherInit(PixelHasher *p, int hash_bits) {
+  int i;
+  p->hash_shift_ = 32 - hash_bits;
+  p->hash_size_ = 1 << hash_bits;
+  p->data_ = (uint32_t *)malloc(p->hash_size_ * sizeof(p->data_[0]));
+  for (i = 0; i < p->hash_size_; ++i) {
+    p->data_[i] = kNotInitialized;
+  }
+}
+
+static void VP8LPixelHasherDelete(PixelHasher *p) {
+  free(p->data_);
+}
+
+static void VP8LPixelHasherInsert(PixelHasher *p, uint32_t argb) {
+  const uint32_t key = (kHashMul * argb) >> p->hash_shift_;
+  p->data_[key] = argb;
+}
+
+static bool VP8LPixelHasherIsInitialized(const PixelHasher *p, uint32_t argb) {
+  const uint32_t key = (kHashMul * argb) >> p->hash_shift_;
+  return p->data_[key] != kNotInitialized;
+}
+
+static bool VP8LPixelHasherContains(const PixelHasher *p, uint32_t argb) {
+  uint32_t key;
+  if (argb == kNotInitialized) {
+    return false;
+  }
+  key = (kHashMul * argb) >> p->hash_shift_;
+  return p->data_[key] == argb;
+}
+
+static bool VP8LPixelHasherLookup(PixelHasher *p,
+                                  uint32_t key, uint32_t* argb) {
+  assert(key < p->hash_size_);
+  if (p->data_[key] != kNotInitialized) {
+    *argb = p->data_[key];
+    return true;
+  }
+  return false;
+}
 
 struct PixelHasherLine {
   void Init(int xsize, int x_downsample_bits, int hash_bits) {
@@ -77,18 +83,18 @@ struct PixelHasherLine {
 
     hashers_ = (PixelHasher *)malloc(hashers_size_ * sizeof(hashers_[0]));
     for (i = 0; i < hashers_size_; ++i) {
-      hashers_[i].Init(hash_bits);
+      VP8LPixelHasherInit(&hashers_[i], hash_bits);
     }
   }
   void Delete() {
     int i;
     for (i = 0; i < hashers_size_; ++i) {
-      hashers_[i].Delete();
+      VP8LPixelHasherDelete(&hashers_[i]);
     }
     free(hashers_);
   }
   void Insert(int x, uint32_t argb) {
-    hashers_[x >> x_downsample_bits_].insert(argb);
+    VP8LPixelHasherInsert(&hashers_[x >> x_downsample_bits_], argb);
   }
   uint32_t GetIndex(uint32_t argb) const {
     uint32_t val = kHashMul * argb;
@@ -98,26 +104,26 @@ struct PixelHasherLine {
   bool Contains(int x, uint32_t argb) {
     int i;
     int ix = x >> x_downsample_bits_;
-    if (hashers_[ix].contains(argb)) {
+    if (VP8LPixelHasherContains(&hashers_[ix], argb)) {
       return true;
     }
-    if (hashers_[ix].is_initialized(argb)) {
+    if (VP8LPixelHasherIsInitialized(&hashers_[ix], argb)) {
       return false;
     }
     for (i = 1; i < hashers_size_; ++i) {
       if (ix - i >= 0) {
-        if (hashers_[ix - i].contains(argb)) {
+        if (VP8LPixelHasherContains(&hashers_[ix - i], argb)) {
           return true;
         }
-        if (hashers_[ix - i].is_initialized(argb)) {
+        if (VP8LPixelHasherIsInitialized(&hashers_[ix - i], argb)) {
           return false;
         }
       }
       if (ix + i < hashers_size_) {
-        if (hashers_[ix + i].contains(argb)) {
+        if (VP8LPixelHasherContains(&hashers_[ix + i], argb)) {
           return true;
         }
-        if (hashers_[ix + i].is_initialized(argb)) {
+        if (VP8LPixelHasherIsInitialized(&hashers_[ix + i], argb)) {
           return false;
         }
       }
@@ -128,16 +134,16 @@ struct PixelHasherLine {
     int i;
     int ix = x >> x_downsample_bits_;
     uint32_t argb;
-    if (hashers_[ix].lookup(hash, &argb)) {
+    if (VP8LPixelHasherLookup(&hashers_[ix], hash, &argb)) {
       return argb;
     }
     for (i = 1; i < hashers_size_; ++i) {
       if (ix - i >= 0 &&
-          hashers_[ix - i].lookup(hash, &argb)) {
+          VP8LPixelHasherLookup(&hashers_[ix - i], hash, &argb)) {
         return argb;
       }
       if (ix + i < hashers_size_ &&
-          hashers_[ix + i].lookup(hash, &argb)) {
+          VP8LPixelHasherLookup(&hashers_[ix + i], hash, &argb)) {
         return argb;
       }
     }
