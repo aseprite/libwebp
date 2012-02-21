8360361077ae9d0617060affc3fff38caedc3c35
Urvang Joshi <urvang@google.com>
2012-02-21 08:44:26 +0000

    Make a few color cache APIs inline
    (performs slightly better for large graphical images)

--- src/utils/color_cache.c
+++ src/utils/color_cache.c
@@ -17,11 +17,6 @@
 extern "C" {
 #endif
 
-static const uint32_t kHashMul = 0x1e35a7bd;
-// kNotInitialized is a special value which can be inserted into the
-// ColorCacheColumn, but is never recalled as a value.
-static const uint32_t kNotInitialized = 0x1e35a7bd;
-
 //------------------------------------------------------------------------------
 // Helper functions.
 
@@ -31,18 +26,8 @@ static WEBP_INLINE uint32_t SubSampleSize(uint32_t size,
   return (size + (1 << sampling_bits) - 1) >> sampling_bits;
 }
 
-static WEBP_INLINE uint32_t GetKey(uint32_t argb, uint32_t hash_shift) {
-  return (kHashMul * argb) >> hash_shift;
-}
-
 //------------------------------------------------------------------------------
-// ColorCacheColumn struct.
-
-struct ColorCacheColumn {
-  uint32_t *data_;
-  uint32_t hash_shift_;
-  uint32_t hash_size_;
-};
+// ColorCacheColumn.
 
 static WEBP_INLINE void ColumnInit(ColorCacheColumn* const cc, int hash_bits) {
   uint32_t i;
@@ -58,12 +43,6 @@ static WEBP_INLINE void ColumnInit(ColorCacheColumn* const cc, int hash_bits) {
 static WEBP_INLINE void ColumnRelease(ColorCacheColumn* const cc) {
   free(cc->data_);
   cc->data_ = NULL;
-}
-
-static WEBP_INLINE void ColumnInsert(ColorCacheColumn* const cc,
-                                     uint32_t argb) {
-  const uint32_t key = GetKey(argb, cc->hash_shift_);
-  cc->data_[key] = argb;
 }
 
 // Returns 1 if the key location in the column contains this ARGB value,
@@ -84,24 +63,8 @@ static WEBP_INLINE int ColumnContains(const ColorCacheColumn* const cc,
   }
 }
 
-static WEBP_INLINE int ColumnLookup(const ColorCacheColumn* const cc,
-                                    uint32_t key, uint32_t* const argb) {
-  assert(key < cc->hash_size_);
-  if (cc->data_[key] != kNotInitialized) {
-    *argb = cc->data_[key];
-    return 1;
-  }
-  return 0;
-}
-
 //------------------------------------------------------------------------------
 // Helper functions for VP8LColorCache.
-
-static WEBP_INLINE int ColorCacheGetColumn(
-    const VP8LColorCache* const color_cache, uint32_t x_pos) {
-  assert(color_cache != NULL);
-  return x_pos >> color_cache->x_downsample_bits_;
-}
 
 static WEBP_INLINE uint32_t ColorCacheGetIndex(
     const VP8LColorCache* const color_cache, uint32_t argb) {
@@ -164,37 +127,6 @@ void VP8LColorCacheRelease(VP8LColorCache* const color_cache) {
     ColumnRelease(&color_cache->cache_columns_[i]);
   }
   free(color_cache->cache_columns_);
-}
-
-void VP8LColorCacheInsert(VP8LColorCache* const color_cache, uint32_t x_pos,
-                          uint32_t argb) {
-  assert(color_cache);
-  {
-    const int col = ColorCacheGetColumn(color_cache, x_pos);
-    ColumnInsert(&color_cache->cache_columns_[col], argb);
-  }
-}
-
-int VP8LColorCacheLookup(const VP8LColorCache* const color_cache,
-                         uint32_t x_pos, uint32_t key, uint32_t* const argb) {
-  int i, col;
-  assert(color_cache != NULL);
-  assert(argb != NULL);
-  col = ColorCacheGetColumn(color_cache, x_pos);
-  if (ColumnLookup(&color_cache->cache_columns_[col], key, argb)) {
-    return 1;
-  }
-  for (i = 1; i < color_cache->num_cache_columns_; ++i) {
-    if (col - i >= 0 &&
-        ColumnLookup(&color_cache->cache_columns_[col - i], key, argb)) {
-      return 1;
-    }
-    if (col + i < color_cache->num_cache_columns_ &&
-        ColumnLookup(&color_cache->cache_columns_[col + i], key, argb)) {
-      return 1;
-    }
-  }
-  return 0;
 }
 
 int VL8LColorCacheInverseLookup(const VP8LColorCache* const color_cache,
--- src/utils/color_cache.h
+++ src/utils/color_cache.h
@@ -19,11 +19,43 @@
 extern "C" {
 #endif
 
-// TODO(urvang): Evaluate if inlining VP8LColorCacheInsert() and/or
-// VP8LColorCacheLookup() gives a better performance.
+//------------------------------------------------------------------------------
+// Helper struct & methods.
 
 // Represents cache for colors in a vertical stripe of an image.
-typedef struct ColorCacheColumn ColorCacheColumn;
+typedef struct {
+  uint32_t *data_;
+  uint32_t hash_shift_;
+  uint32_t hash_size_;
+} ColorCacheColumn;
+
+static const uint32_t kHashMul = 0x1e35a7bd;
+// kNotInitialized is a special value which can be inserted into the
+// ColorCacheColumn, but is never recalled as a value.
+static const uint32_t kNotInitialized = 0x1e35a7bd;
+
+static WEBP_INLINE uint32_t GetKey(uint32_t argb, uint32_t hash_shift) {
+  return (kHashMul * argb) >> hash_shift;
+}
+
+static WEBP_INLINE int ColumnLookup(const ColorCacheColumn* const cc,
+                                    uint32_t key, uint32_t* const argb) {
+  assert(key < cc->hash_size_);
+  if (cc->data_[key] != kNotInitialized) {
+    *argb = cc->data_[key];
+    return 1;
+  }
+  return 0;
+}
+
+static WEBP_INLINE void ColumnInsert(ColorCacheColumn* const cc,
+                                     uint32_t argb) {
+  const uint32_t key = GetKey(argb, cc->hash_shift_);
+  cc->data_[key] = argb;
+}
+
+//------------------------------------------------------------------------------
+// Main APIs.
 
 // Main color cache struct. It contains multiple cache columns.
 typedef struct {
@@ -32,6 +64,12 @@ typedef struct {
   int hash_bits_;
   int x_downsample_bits_;
 } VP8LColorCache;
+
+static WEBP_INLINE int ColorCacheGetColumn(
+    const VP8LColorCache* const color_cache, uint32_t x_pos) {
+  assert(color_cache != NULL);
+  return x_pos >> color_cache->x_downsample_bits_;
+}
 
 // Initializes the color cache based on the given parameters.
 // Note: 'color_cache' must already be allocated.
@@ -45,19 +83,44 @@ void VP8LColorCacheRelease(VP8LColorCache* const color_cache);
 // TODO(urvang): Use 'argb_t' type for 'argb' once 'argb_t' is part of
 // webp/types.h
 // Given the ARGB value and x position of a pixel, insert it into the cache.
-void VP8LColorCacheInsert(VP8LColorCache* const color_cache, uint32_t x_pos,
-                          uint32_t argb);
+static WEBP_INLINE void VP8LColorCacheInsert(VP8LColorCache* const color_cache,
+                                             uint32_t x_pos, uint32_t argb) {
+  const int col = ColorCacheGetColumn(color_cache, x_pos);
+  ColumnInsert(&color_cache->cache_columns_[col], argb);
+}
 
 // Given the key and x position of a pixel, find out its ARGB value.
 // Returns 1 if the ARGB value was found.
-int VP8LColorCacheLookup(const VP8LColorCache* const color_cache,
-                         uint32_t x_pos, uint32_t key, uint32_t* const argb);
+static WEBP_INLINE int VP8LColorCacheLookup(
+    const VP8LColorCache* const color_cache, uint32_t x_pos, uint32_t key,
+    uint32_t* const argb) {
+  int i, col;
+  assert(color_cache != NULL);
+  assert(argb != NULL);
+  col = ColorCacheGetColumn(color_cache, x_pos);
+  if (ColumnLookup(&color_cache->cache_columns_[col], key, argb)) {
+    return 1;
+  }
+  for (i = 1; i < color_cache->num_cache_columns_; ++i) {
+    if (col - i >= 0 &&
+        ColumnLookup(&color_cache->cache_columns_[col - i], key, argb)) {
+      return 1;
+    }
+    if (col + i < color_cache->num_cache_columns_ &&
+        ColumnLookup(&color_cache->cache_columns_[col + i], key, argb)) {
+      return 1;
+    }
+  }
+  return 0;
+}
 
 // Given the ARGB value and x position of a pixel, find out its key.
 // Returns 1 if the key was found.
 int VL8LColorCacheInverseLookup(const VP8LColorCache* const color_cache,
                                 uint32_t x_pos, uint32_t argb,
                                 uint32_t* const key);
+
+//------------------------------------------------------------------------------
 
 #if defined(__cplusplus) || defined(c_plusplus)
 }
