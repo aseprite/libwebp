602c001227413c8e459283ef7c7ca7ee73871afa
James Zern <jzern@google.com>
2012-03-30 15:26:07 -0700

    cosmetics
    long line, debug warning, remove out of date TODO
    
    Change-Id: Ic8a40c9d731178af85645b3e24c1cbd807d7d58b

--- src/dec/webp.c
+++ src/dec/webp.c
@@ -259,7 +259,7 @@ VP8StatusCode WebPParseHeaders(const uint8_t** data, uint32_t* data_size,
   }
 
   *bytes_skipped = (uint32_t)(buf - *data);
-  assert(buf - *data < MAX_CHUNK_PAYLOAD);
+  assert((uint64_t)(buf - *data) < MAX_CHUNK_PAYLOAD);
   assert(*bytes_skipped == *data_size - buf_size);
   *data = buf;
   *data_size = buf_size;
