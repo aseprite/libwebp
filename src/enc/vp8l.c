2618053c4c4fee2c98a304409c6e2c2c37023699
Urvang Joshi <urvang@google.com>
2012-05-14 11:19:00 +0530

    Correct size in VP8L header.
    The size written in VP8L header should be without padding.
    (Also clarified this code using consts).
    
    Change-Id: Ic6583d760c0f52ef61924ab0330c65c668a12fdc

--- src/enc/vp8l.c
+++ src/enc/vp8l.c
@@ -791,15 +791,15 @@ static void PutLE32(uint8_t* const data, uint32_t val) {
 }
 
 static WebPEncodingError WriteRiffHeader(const VP8LEncoder* const enc,
-                                         size_t riff_size, size_t vp8l_size) {
+                                         size_t riff_size, size_t vp8l_size,
+                                         size_t pad) {
   const WebPPicture* const pic = enc->pic_;
   uint8_t riff[HEADER_SIZE + SIGNATURE_SIZE] = {
     'R', 'I', 'F', 'F', 0, 0, 0, 0, 'W', 'E', 'B', 'P',
     'V', 'P', '8', 'L', 0, 0, 0, 0, LOSSLESS_MAGIC_BYTE,
   };
-  if (riff_size < (vp8l_size + TAG_SIZE + CHUNK_HEADER_SIZE)) {
-    return VP8_ENC_ERROR_INVALID_CONFIGURATION;
-  }
+  (void)pad;  // Silence an "unused variable" warning.
+  assert(riff_size >= (TAG_SIZE + CHUNK_HEADER_SIZE + vp8l_size + pad));
   PutLE32(riff + TAG_SIZE, (uint32_t)riff_size);
   PutLE32(riff + RIFF_HEADER_SIZE + TAG_SIZE, (uint32_t)vp8l_size);
   if (!pic->writer(riff, sizeof(riff), pic)) {
@@ -821,18 +821,15 @@ static void WriteImageSize(VP8LEncoder* const enc, VP8LBitWriter* const bw) {
 static WebPEncodingError WriteImage(const VP8LEncoder* const enc,
                                     VP8LBitWriter* const bw,
                                     size_t* const coded_size) {
-  size_t riff_size, vp8l_size, webpll_size, pad;
   const WebPPicture* const pic = enc->pic_;
   WebPEncodingError err = VP8_ENC_OK;
   const uint8_t* const webpll_data = VP8LBitWriterFinish(bw);
+  const size_t webpll_size = VP8LBitWriterNumBytes(bw);
+  const size_t vp8l_size = SIGNATURE_SIZE + webpll_size;
+  const size_t pad = vp8l_size & 1;
+  const size_t riff_size = TAG_SIZE + CHUNK_HEADER_SIZE + vp8l_size + pad;
 
-  webpll_size = VP8LBitWriterNumBytes(bw);
-  vp8l_size = SIGNATURE_SIZE + webpll_size;
-  pad = vp8l_size & 1;
-  vp8l_size += pad;
-
-  riff_size = TAG_SIZE + CHUNK_HEADER_SIZE + vp8l_size;
-  err = WriteRiffHeader(enc, riff_size, vp8l_size);
+  err = WriteRiffHeader(enc, riff_size, vp8l_size, pad);
   if (err != VP8_ENC_OK) goto Error;
 
   if (!pic->writer(webpll_data, webpll_size, pic)) {
