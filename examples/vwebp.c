e25ee71a1e487da44ce99654f680bbaa7a41a059
Urvang Joshi <urvang@google.com>
2012-08-23 16:28:36 +0530

    Mux API change:
    Create common APIs for image, frame and tile.
    
    Change-Id: I709ad752133094bd5bc89dd9c832ff79802aac68

--- examples/vwebp.c
+++ examples/vwebp.c
@@ -1,19 +1,19 @@
-// Copyright 2011 Google Inc. All Rights Reserved.
-//
-// This code is licensed under the same terms as WebM:
-//  Software License Agreement:  http://www.webmproject.org/license/software/
-//  Additional IP Rights Grant:  http://www.webmproject.org/license/additional/
-// -----------------------------------------------------------------------------
-//
-//  Simple WebP file viewer.
-//
-// Compiling on linux:
-//   sudo apt-get install libglut3-dev mesa-common-dev
-//   gcc -o vwebp vwebp.c -O3 -lwebp -lwebpmux -lglut -lGL -lpthread -lm
-// Compiling on Mac + XCode:
-//   gcc -o vwebp vwebp.c -lwebp -lwebpmux -framework GLUT -framework OpenGL
-//
-// Author: Skal (pascal.massimino@gmail.com)
+  // Copyright 2011 Google Inc. All Rights Reserved.
+  //
+  // This code is licensed under the same terms as WebM:
+  //  Software License Agreement:  http://www.webmproject.org/license/software/
+  //  Additional IP Rights Grant:  http://www.webmproject.org/license/additional/
+  // -----------------------------------------------------------------------------
+  //
+  //  Simple WebP file viewer.
+  //
+  // Compiling on linux:
+  //   sudo apt-get install libglut3-dev mesa-common-dev
+  //   gcc -o vwebp vwebp.c -O3 -lwebp -lwebpmux -lglut -lGL -lpthread -lm
+  // Compiling on Mac + XCode:
+  //   gcc -o vwebp vwebp.c -lwebp -lwebpmux -framework GLUT -framework OpenGL
+  //
+  // Author: Skal (pascal.massimino@gmail.com)
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -37,143 +37,143 @@
 #define snprintf _snprintf
 #endif
 
-static void Help(void);
+  static void Help(void);
 
-// Unfortunate global variables. Gathered into a struct for comfort.
-static struct {
-  int has_animation;
-  int done;
-  int decoding_error;
-  int print_info;
+  // Unfortunate global variables. Gathered into a struct for comfort.
+  static struct {
+    int has_animation;
+    int done;
+    int decoding_error;
+    int print_info;
 
-  int loop_count;
+    int loop_count;
 
-  const char* file_name;
-  WebPData data;
-  WebPDecoderConfig* config;
-  const WebPDecBuffer* pic;
-  WebPDemuxer* dmux;
-  WebPIterator frameiter;
-} kParams;
+    const char* file_name;
+    WebPData data;
+    WebPDecoderConfig* config;
+    const WebPDecBuffer* pic;
+    WebPDemuxer* dmux;
+    WebPIterator frameiter;
+  } kParams;
 
-static void ClearPreviousPic(void) {
-  WebPFreeDecBuffer((WebPDecBuffer*)kParams.pic);
-  kParams.pic = NULL;
-}
+  static void ClearPreviousPic(void) {
+    WebPFreeDecBuffer((WebPDecBuffer*)kParams.pic);
+    kParams.pic = NULL;
+  }
 
-static void ClearParams(void) {
-  ClearPreviousPic();
-  WebPDataClear(&kParams.data);
-  WebPDemuxReleaseIterator(&kParams.frameiter);
-  WebPDemuxDelete(kParams.dmux);
-  kParams.dmux = NULL;
-}
+  static void ClearParams(void) {
+    ClearPreviousPic();
+    WebPDataClear(&kParams.data);
+    WebPDemuxReleaseIterator(&kParams.frameiter);
+    WebPDemuxDelete(kParams.dmux);
+    kParams.dmux = NULL;
+  }
 
-//------------------------------------------------------------------------------
-// Callbacks
+  //------------------------------------------------------------------------------
+  // Callbacks
 
-static void HandleKey(unsigned char key, int pos_x, int pos_y) {
-  (void)pos_x;
-  (void)pos_y;
-  if (key == 'q' || key == 'Q' || key == 27 /* Esc */) {
+  static void HandleKey(unsigned char key, int pos_x, int pos_y) {
+    (void)pos_x;
+    (void)pos_y;
+    if (key == 'q' || key == 'Q' || key == 27 /* Esc */) {
 #ifdef FREEGLUT
-    glutLeaveMainLoop();
+      glutLeaveMainLoop();
 #else
-    ClearParams();
-    exit(0);
+      ClearParams();
+      exit(0);
 #endif
-  } else if (key == 'i') {
-    kParams.print_info = 1 - kParams.print_info;
-    glutPostRedisplay();
-  }
-}
-
-static void HandleReshape(int width, int height) {
-  // TODO(skal): proper handling of resize, esp. for large pictures.
-  // + key control of the zoom.
-  glViewport(0, 0, width, height);
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  glMatrixMode(GL_MODELVIEW);
-  glLoadIdentity();
-}
-
-static void PrintString(const char* const text) {
-  void* const font = GLUT_BITMAP_9_BY_15;
-  int i;
-  for (i = 0; text[i]; ++i) {
-    glutBitmapCharacter(font, text[i]);
-  }
-}
-
-static void DrawCheckerBoard(void) {
-  const int square_size = 8;  // must be a power of 2
-  int x, y;
-  GLint viewport[4];  // x, y, width, height
-
-  glPushMatrix();
-
-  glGetIntegerv(GL_VIEWPORT, viewport);
-  // shift to integer coordinates with (0,0) being top-left.
-  glOrtho(0, viewport[2], viewport[3], 0, -1, 1);
-  for (y = 0; y < viewport[3]; y += square_size) {
-    for (x = 0; x < viewport[2]; x += square_size) {
-      const GLubyte color = 128 + 64 * (!((x + y) & square_size));
-      glColor3ub(color, color, color);
-      glRecti(x, y, x + square_size, y + square_size);
+    } else if (key == 'i') {
+      kParams.print_info = 1 - kParams.print_info;
+      glutPostRedisplay();
     }
   }
-  glPopMatrix();
-}
 
-static void HandleDisplay(void) {
-  const WebPDecBuffer* pic = kParams.pic;
-  if (pic == NULL) return;
-  glClear(GL_COLOR_BUFFER_BIT);
-  glPushMatrix();
-  glPixelZoom(1, -1);
-  glRasterPos2f(-1, 1);
-  glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-  glPixelStorei(GL_UNPACK_ROW_LENGTH, pic->u.RGBA.stride / 4);
-  DrawCheckerBoard();
-  glDrawPixels(pic->width, pic->height,
-               GL_RGBA, GL_UNSIGNED_BYTE,
-               (GLvoid*)pic->u.RGBA.rgba);
-  if (kParams.print_info) {
-    char tmp[32];
-
-    glColor4f(0.0, 0.0, 0.0, 1.0);
-    glRasterPos2f(-0.95f, 0.90f);
-    PrintString(kParams.file_name);
-
-    snprintf(tmp, sizeof(tmp), "Dimension:%d x %d", pic->width, pic->height);
-    glColor4f(0.0, 0.0, 0.0, 1.0);
-    glRasterPos2f(-0.95f, 0.80f);
-    PrintString(tmp);
+  static void HandleReshape(int width, int height) {
+    // TODO(skal): proper handling of resize, esp. for large pictures.
+    // + key control of the zoom.
+    glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
   }
-  glPopMatrix();
-  glFlush();
-}
 
-static void StartDisplay(const WebPDecBuffer* const pic) {
-  glutInitDisplayMode(GLUT_RGBA);
-  glutInitWindowSize(pic->width, pic->height);
-  glutCreateWindow("WebP viewer");
-  glutDisplayFunc(HandleDisplay);
-  glutIdleFunc(NULL);
-  glutKeyboardFunc(HandleKey);
-  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-  glEnable(GL_BLEND);
-  glClearColor(0.0, 0.0, 0.0, 0.0);
-  HandleReshape(pic->width, pic->height);
-}
+  static void PrintString(const char* const text) {
+    void* const font = GLUT_BITMAP_9_BY_15;
+    int i;
+    for (i = 0; text[i]; ++i) {
+      glutBitmapCharacter(font, text[i]);
+    }
+  }
 
-//------------------------------------------------------------------------------
-// File decoding
+  static void DrawCheckerBoard(void) {
+    const int square_size = 8;  // must be a power of 2
+    int x, y;
+    GLint viewport[4];  // x, y, width, height
 
-static int Decode(int* const duration) {
-  const WebPIterator* const iter = &kParams.frameiter;
-  WebPDecoderConfig* const config = kParams.config;
+    glPushMatrix();
+
+    glGetIntegerv(GL_VIEWPORT, viewport);
+    // shift to integer coordinates with (0,0) being top-left.
+    glOrtho(0, viewport[2], viewport[3], 0, -1, 1);
+    for (y = 0; y < viewport[3]; y += square_size) {
+      for (x = 0; x < viewport[2]; x += square_size) {
+        const GLubyte color = 128 + 64 * (!((x + y) & square_size));
+        glColor3ub(color, color, color);
+        glRecti(x, y, x + square_size, y + square_size);
+      }
+    }
+    glPopMatrix();
+  }
+
+  static void HandleDisplay(void) {
+    const WebPDecBuffer* pic = kParams.pic;
+    if (pic == NULL) return;
+    glClear(GL_COLOR_BUFFER_BIT);
+    glPushMatrix();
+    glPixelZoom(1, -1);
+    glRasterPos2f(-1, 1);
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+    glPixelStorei(GL_UNPACK_ROW_LENGTH, pic->u.RGBA.stride / 4);
+    DrawCheckerBoard();
+    glDrawPixels(pic->width, pic->height,
+                 GL_RGBA, GL_UNSIGNED_BYTE,
+                 (GLvoid*)pic->u.RGBA.rgba);
+    if (kParams.print_info) {
+      char tmp[32];
+
+      glColor4f(0.0, 0.0, 0.0, 1.0);
+      glRasterPos2f(-0.95f, 0.90f);
+      PrintString(kParams.file_name);
+
+      snprintf(tmp, sizeof(tmp), "Dimension:%d x %d", pic->width, pic->height);
+      glColor4f(0.0, 0.0, 0.0, 1.0);
+      glRasterPos2f(-0.95f, 0.80f);
+      PrintString(tmp);
+    }
+    glPopMatrix();
+    glFlush();
+  }
+
+  static void StartDisplay(const WebPDecBuffer* const pic) {
+    glutInitDisplayMode(GLUT_RGBA);
+    glutInitWindowSize(pic->width, pic->height);
+    glutCreateWindow("WebP viewer");
+    glutDisplayFunc(HandleDisplay);
+    glutIdleFunc(NULL);
+    glutKeyboardFunc(HandleKey);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glEnable(GL_BLEND);
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    HandleReshape(pic->width, pic->height);
+  }
+
+  //------------------------------------------------------------------------------
+  // File decoding
+
+  static int Decode(int* const duration) {
+    const WebPIterator* const iter = &kParams.frameiter;
+    WebPDecoderConfig* const config = kParams.config;
   WebPDecBuffer* const output_buffer = &config->output;
   int ok = 0;
 
