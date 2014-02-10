0238f5767a3020b02aeebbea3c7ce32460b18e61
skal <pascal.massimino@gmail.com>
2014-02-10 12:34:16 -0800

    add some fix for endianness
    e.g: mips-gcc doesn't define __BIG_ENDIAN__
    
    Change-Id: Ic06bf453164ddddc69a523e7845a4993e14a1af2

--- src/utils/bit_reader.h
+++ src/utils/bit_reader.h
@@ -86,6 +86,15 @@ extern "C" {
 
 #endif
 
+// some endian fix (e.g.: mips-gcc doesn't define __BIG_ENDIAN__)
+#if !defined(__BIG_ENDIAN__) && !defined(__LITTLE_ENDIAN__)
+#if defined(__BYTE_ORDER__) && (__BYTE_ORDER__ == __ORDER_BIG_ENDIAN__)
+#define __BIG_ENDIAN__
+#else   // otherwise, assume __LITTLE_ENDIAN__
+#define __LITTLE_ENDIAN__
+#endif  // __BYTE_ORDER__
+#endif  // !__BIG_ENDIAN__ && !__LITTLE_ENDIAN__
+
 //------------------------------------------------------------------------------
 // Derived types and constants
 
