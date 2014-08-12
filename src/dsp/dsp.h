592b719c77555a96b295f9aba81a6512e75853f4
Djordje Pesut <djordje.pesut@imgtec.com>
2014-08-12 11:47:36 +0200

    MIPS: dspr2: Optimization of some simple point-sampling functions
    Change-Id: I6a4ab29bd0cc5a2951a8882cf9997032dc38bd79

--- src/dsp/dsp.h
+++ src/dsp/dsp.h
@@ -64,6 +64,9 @@ extern "C" {
 #define WEBP_USE_MIPS32
 #if (__mips_isa_rev >= 2)
 #define WEBP_USE_MIPS32_R2
+#if defined(__mips_dspr2) || (__mips_dsp_rev >= 2)
+#define WEBP_USE_MIPS_DSP_R2
+#endif
 #endif
 #endif
 
