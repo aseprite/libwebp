b597af563c241a45480feacaf3d384d9192b84d7
levytamar82 <tamar.levy@intel.com>
2014-08-14 10:17:26 -0700

    PickBestIntra4 for AVX2
    The inner loop of PickBestIntra4 can now handle two iteration at a time using
    AVX2.
    every iteration process 4 bytes and now it process 8 bytes.
    This commit is only part of the change which include all the AVX2 functions
    that being called directly or indirectly from PickBestIntra4.
    There is a 24% function level gain and 12% user level gain.
    +
    added Part II of PickBestIntra4 that include the main function.
    
    Change-Id: I56f2675b7aeb9978145d9f130d6fa1a74a07f218

--- src/dsp/enc.c
+++ src/dsp/enc.c
@@ -673,6 +673,14 @@ static WEBP_INLINE void Copy(const uint8_t* src, uint8_t* dst, int size) {
 
 static void Copy4x4(const uint8_t* src, uint8_t* dst) { Copy(src, dst, 4); }
 
+void Copy4x4x2(const uint8_t* src, uint8_t* dst) { 
+  int y;
+  for (y = 0; y < 4; ++y) {
+    memcpy(dst, src, 4);
+    src += 32;
+    dst += BPS;
+  }
+}
 //------------------------------------------------------------------------------
 // Initialization
 
@@ -680,7 +688,9 @@ static void Copy4x4(const uint8_t* src, uint8_t* dst) { Copy(src, dst, 4); }
 // implementations within VP8EncDspInit().
 VP8CHisto VP8CollectHistogram;
 VP8Idct VP8ITransform;
+VP8Idct VP8ITransform4x2;
 VP8Fdct VP8FTransform;
+VP8Fdct VP8FTransform4x2;
 VP8WHT VP8FTransformWHT;
 VP8Intra4Preds VP8EncPredLuma4;
 VP8IntraPreds VP8EncPredLuma16;
@@ -689,10 +699,13 @@ VP8Metric VP8SSE16x16;
 VP8Metric VP8SSE8x8;
 VP8Metric VP8SSE16x8;
 VP8Metric VP8SSE4x4;
+VP8Metric2 VP8SSE4x4x2;
 VP8WMetric VP8TDisto4x4;
+VP8WMetric2 VP8TDisto4x4x2;
 VP8WMetric VP8TDisto16x16;
 VP8QuantizeBlock VP8EncQuantizeBlock;
 VP8Quantize2Blocks VP8EncQuantize2Blocks;
+VP8QuantizeBlock4x2 VP8EncQuantizeBlock4x2;
 VP8QuantizeBlockWHT VP8EncQuantizeBlockWHT;
 VP8BlockCopy VP8Copy4x4;
 
