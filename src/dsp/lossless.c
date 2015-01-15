d4a4c7fb68a92ce940c1d1a3d088fa9ba032b8c9
Pascal Massimino <pascal.massimino@gmail.com>
2015-01-15 07:31:46 -0800

    simplify/reorganize arguments for CollectColorBlueTransforms
    Change-Id: Icb8f828dfe25672662de18d0e48e7d3144b1f38d

--- src/dsp/lossless.c
+++ src/dsp/lossless.c
@@ -1067,20 +1067,31 @@ static float PredictionCostCrossColor(const int accumulated[256],
          PredictionCostSpatial(counts, 3, kExpValue);
 }
 
+static void CollectColorRedTransforms(const uint32_t* argb, int stride,
+                                      int tile_width, int tile_height,
+                                      int green_to_red, int histo[]) {
+  while (tile_height-- > 0) {
+    int x;
+    for (x = 0; x < tile_width; ++x) {
+      ++histo[TransformColorRed(green_to_red, argb[x])];
+    }
+    argb += stride;
+  }
+}
+
 static float GetPredictionCostCrossColorRed(
     int tile_x_offset, int tile_y_offset, int all_x_max, int all_y_max,
     int xsize, VP8LMultipliers prev_x, VP8LMultipliers prev_y, int green_to_red,
     const int accumulated_red_histo[256], const uint32_t* const argb) {
-  int all_y;
   int histo[256] = { 0 };
   float cur_diff;
-  for (all_y = tile_y_offset; all_y < all_y_max; ++all_y) {
-    int ix = all_y * xsize + tile_x_offset;
-    int all_x;
-    for (all_x = tile_x_offset; all_x < all_x_max; ++all_x, ++ix) {
-      ++histo[TransformColorRed(green_to_red, argb[ix])];  // red.
-    }
-  }
+
+  VP8LCollectColorRedTransforms(argb + tile_y_offset * xsize + tile_x_offset,
+                                xsize,
+                                all_x_max - tile_x_offset,
+                                all_y_max - tile_y_offset,
+                                green_to_red, histo);
+
   cur_diff = PredictionCostCrossColor(accumulated_red_histo, histo);
   if ((uint8_t)green_to_red == prev_x.green_to_red_) {
     cur_diff -= 3;  // favor keeping the areas locally similar
@@ -1133,17 +1144,16 @@ static void GetBestGreenToRed(
   best_tx->green_to_red_ = green_to_red;
 }
 
-static void CollectColorBlueTransforms(
-    int tile_x_offset, int tile_y_offset, int all_x_max, int all_y_max,
-    int xsize, int green_to_blue, int red_to_blue, int* histo,
-    const uint32_t* const argb) {
-  int all_y;
-  for (all_y = tile_y_offset; all_y < all_y_max; ++all_y) {
-    int all_x;
-    int ix = all_y * xsize + tile_x_offset;
-    for (all_x = tile_x_offset; all_x < all_x_max; ++all_x, ++ix) {
-      ++histo[TransformColorBlue(green_to_blue, red_to_blue, argb[ix])];
+static void CollectColorBlueTransforms(const uint32_t* argb, int stride,
+                                       int tile_width, int tile_height,
+                                       int green_to_blue, int red_to_blue,
+                                       int histo[]) {
+  while (tile_height-- > 0) {
+    int x;
+    for (x = 0; x < tile_width; ++x) {
+      ++histo[TransformColorBlue(green_to_blue, red_to_blue, argb[x])];
     }
+    argb += stride;
   }
 }
 
@@ -1155,9 +1165,11 @@ static float GetPredictionCostCrossColorBlue(
   int histo[256] = { 0 };
   float cur_diff;
 
-  VP8LCollectColorBlueTransforms(tile_x_offset, tile_y_offset, all_x_max,
-                                 all_y_max, xsize, green_to_blue, red_to_blue,
-                                 histo, argb);
+  VP8LCollectColorBlueTransforms(argb + tile_y_offset * xsize + tile_x_offset,
+                                 xsize,
+                                 all_x_max - tile_x_offset,
+                                 all_y_max - tile_y_offset,
+                                 green_to_blue, red_to_blue, histo);
 
   cur_diff = PredictionCostCrossColor(accumulated_blue_histo, histo);
   if ((uint8_t)green_to_blue == prev_x.green_to_blue_) {
@@ -1738,6 +1750,7 @@ VP8LConvertFunc VP8LConvertBGRAToRGB565;
 VP8LConvertFunc VP8LConvertBGRAToBGR;
 
 VP8LCollectColorBlueTransformsFunc VP8LCollectColorBlueTransforms;
+VP8LCollectColorRedTransformsFunc VP8LCollectColorRedTransforms;
 
 VP8LFastLog2SlowFunc VP8LFastLog2Slow;
 VP8LFastLog2SlowFunc VP8LFastSLog2Slow;
@@ -1779,6 +1792,7 @@ WEBP_TSAN_IGNORE_FUNCTION void VP8LDspInit(void) {
   VP8LConvertBGRAToBGR = VP8LConvertBGRAToBGR_C;
 
   VP8LCollectColorBlueTransforms = CollectColorBlueTransforms;
+  VP8LCollectColorRedTransforms = CollectColorRedTransforms;
 
   VP8LFastLog2Slow = FastLog2Slow;
   VP8LFastSLog2Slow = FastSLog2Slow;
