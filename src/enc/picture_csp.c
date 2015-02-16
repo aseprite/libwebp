e18ee50e7b51c01f0ac9dba5cc2cc1e687dc62fa
Pascal Massimino <pascal.massimino@gmail.com>
2015-02-16 02:47:16 -0800

    import rgb565 and rgb4444 functions
    note: input ordering depends on WEBP_SWAP_16BIT_CSP
    
    Change-Id: I5ce8eaf6bf63a28b81fa2288bf8a697c7d56a5ed

--- src/enc/picture_csp.c
+++ src/enc/picture_csp.c
@@ -1115,4 +1115,68 @@ int WebPImportGray(const uint8_t* gray_data, WebPPicture* pic) {
   return 1;
 }
 
+int WebPImportRGB565(const uint8_t* rgb565, WebPPicture* pic) {
+  int x, y;
+  if (pic == NULL || rgb565 == NULL) return 0;
+  pic->colorspace = WEBP_YUV420;
+  pic->use_argb = 1;
+  if (!WebPPictureAlloc(pic)) return 0;
+  for (y = 0; y < pic->height; ++y) {
+    const int width = pic->width;
+    uint32_t* dst = pic->argb + y * pic->argb_stride;
+    for (x = 0; x < width; ++x) {
+#ifdef WEBP_SWAP_16BIT_CSP
+      const uint32_t rg = rgb565[2 * x + 1];
+      const uint32_t gb = rgb565[2 * x + 0];
+#else
+      const uint32_t rg = rgb565[2 * x + 0];
+      const uint32_t gb = rgb565[2 * x + 1];
+#endif
+      uint32_t r = rg & 0xf8;
+      uint32_t g = ((rg << 5) | (gb >> 3)) & 0xfc;
+      uint32_t b = (gb << 5);
+      // dithering
+      r = r | (r >> 5);
+      g = g | (g >> 6);
+      b = b | (b >> 5);
+      dst[x] = MakeARGB32(r, g, b);
+    }
+    rgb565 += 2 * width;
+  }
+  return 1;
+}
+
+int WebPImportRGB4444(const uint8_t* rgb4444, WebPPicture* pic) {
+  int x, y;
+  if (pic == NULL || rgb4444 == NULL) return 0;
+  pic->colorspace = WEBP_YUV420;
+  pic->use_argb = 1;
+  if (!WebPPictureAlloc(pic)) return 0;
+  for (y = 0; y < pic->height; ++y) {
+    const int width = pic->width;
+    uint32_t* dst = pic->argb + y * pic->argb_stride;
+    for (x = 0; x < width; ++x) {
+#ifdef WEBP_SWAP_16BIT_CSP
+      const uint32_t rg = rgb4444[2 * x + 1];
+      const uint32_t ba = rgb4444[2 * x + 0];
+#else
+      const uint32_t rg = rgb4444[2 * x + 0];
+      const uint32_t ba = rgb4444[2 * x + 1];
+#endif
+      uint32_t r = rg & 0xf0;
+      uint32_t g = (rg << 4);
+      uint32_t b = (ba & 0xf0);
+      uint32_t a = (ba << 4);
+      // dithering
+      r = r | (r >> 4);
+      g = g | (g >> 4);
+      b = b | (b >> 4);
+      a = a | (a >> 4);
+      dst[x] = (a << 24) | (r << 16) | (g << 8) | b;
+    }
+    rgb4444 += 2 * width;
+  }
+  return 1;
+}
+
 //------------------------------------------------------------------------------
